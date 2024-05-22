f575cfe37c664c7d5e15a21c5589b809677521c9
Yurii-Yevsiukov <92231@ukr.net>
2024-05-22 19:03:20 +0300

    update FriendListItem

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -4,12 +4,9 @@ import css from './FriendListItem.module.css';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      {isOnline ? (
-        <span className={css.online}>{isOnline}</span>
-      ) : (
-        <span className={css.status}>{isOnline}</span>
-      )}
-
+      <span className={isOnline ? css.statusOnline : css.statusOffline}>
+        {isOnline}
+      </span>
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
     </li>
